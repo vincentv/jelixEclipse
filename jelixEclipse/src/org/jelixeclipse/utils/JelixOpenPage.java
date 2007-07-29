b0c0b8bd56b716a33f21df2b2bd880d9004b7ea3
pokito31 <pokito31@un.known>
2007-07-29 17:57:35 +0000

    29/07 [TGI] Classe permettant d'ouvrir un fichier ds l'éditeur eclipse

---
+++ jelixEclipse/src/org/jelixeclipse/utils/JelixOpenPage.java
@@ -0,0 +1,24 @@
+package org.jelixeclipse.utils;
+
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.core.resources.IFile;
+
+
+public class JelixOpenPage {
+	public static void Open(Wizard w, final IFile file){
+		w.getShell().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage page =
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				try {
+					IDE.openEditor(page, file, true);
+				} catch (PartInitException e) {
+				}
+			}
+		});
+	}
+}
